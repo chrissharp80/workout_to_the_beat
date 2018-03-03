98d4bb42b1aa2a77752735d7d88c1935f6e6e61c
sdowdy70 <sdowdy70@icloud.com>
2018-03-03 16:13:09 -0500

    app setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render(index.html)
+});
+
+app.listen(3000, function() {
+    console.log("Listening on Port 3000")
+});
