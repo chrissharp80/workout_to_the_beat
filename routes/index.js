75e6531ea6de42de143d521e39702daee401e093
Chris Sharp <lcsharp@us.ibm.com>
2018-03-03 18:49:37 -0500

    add picture and customize some code for this site

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// capture data emitted from Arduino
-let message = 'hello';
-
-
 // get home page
 router.get('/', (req, res, next) => {
     res.render('index', {
-		title: 'Home',
-		message: message
+		title: 'Home'
     });
 });
-
-router.get('/message', (req, res) => {
-    res.status(200).send({message: message});
-})
 
 module.exports = router;
