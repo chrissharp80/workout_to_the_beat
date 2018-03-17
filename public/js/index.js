0e4989c8a2d4cd3e13816bda15346fe596a4582b
Chris Sharp <chrissharp80@gmail.com>
2018-03-17 19:31:34 -0400

    Fix dependency bugs and add spaces between labels and drop downs in index.pug (#9)
    * add nodemon to package.json for autostart on change
    
    * fix bugs with css and js imports and modify layout of index.pug
    
    * addressed review comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,3 @@
-function updateMessage(message) {
-	$('#message').text(message);
-}
-
-function getMessage() {
-	url = '/message';
-	request = $.ajax({
-		url: url,
-		method: 'GET'
-	});
-	request.done(function (msg) {
-		console.log(msg);
-		updateMessage(msg.message);
-	});
-	request.fail(function (msg) {
-		console.log('ERROR: Could not get the LBs');
-	});
-}
-
 $(document).ready(function() {
-	setInterval(getMessage, 1000);
+	// put initial JS call here
 });
