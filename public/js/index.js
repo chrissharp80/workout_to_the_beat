666a209140525245ac842d842de94d592fb4cc8a
Chris Sharp <chrissharp80@gmail.com>
2018-03-17 22:58:39 -0400

    listener on submit button (#13)

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	// put initial JS call here
+	$('#submitInfoForm').on('click', (() => {
+		console.log($('#warmup')[0].value);
+		// alert('form will be handled');
+	}));
+
 });
